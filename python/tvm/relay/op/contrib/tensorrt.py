9581e858e0b7a884415956e2cfe8e2c1f3a79677
David Huang <davhuan@amazon.com>
2021-07-15 11:34:03 -0700

    Add dense op to TRT explicit batch

--- python/tvm/relay/op/contrib/tensorrt.py
+++ python/tvm/relay/op/contrib/tensorrt.py
@@ -177,7 +177,7 @@ def check_dynamism(args, op_name):
     """
     for arg in args:
         if isinstance(arg, (Call, Var, Constant, TupleGetItem)):
-            if op_name not in ["nn.conv2d", "nn.conv2d_transpose"] and not get_tensorrt_use_implicit_batch_mode():
+            if op_name not in ["nn.conv2d", "nn.conv2d_transpose", "nn.dense"] and not get_tensorrt_use_implicit_batch_mode():
                 # Explicit batch mode doesn't support any dynamic shapes yet, except for conv2d
                 for dim_shape in arg.checked_type.shape:
                     if isinstance(dim_shape, tvm.tir.expr.Any):
